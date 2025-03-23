382ad8e9f64f800305c07af1e7a4b6bcc52e4fbc
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-23 15:17:07 -0500

    init

---
+++ src/test/java/com/isai/login_spring_boot/LoginSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.login_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
