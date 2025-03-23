937b2832eb4f35aa25f9ad4962e00e04a0f3bddc
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-23 15:37:38 -0500

    relacion entre usuario y rol

--- src/main/java/com/isai/login_spring_boot/models/User.java
+++ src/main/java/com/isai/login_spring_boot/models/User.java
@@ -4,10 +4,15 @@ import java.util.Collection;
 
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotEmpty;
@@ -42,6 +47,8 @@ public class User {
     @Length(max = 30)
     private String password;
 
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "userId"), inverseJoinColumns = @JoinColumn(name = "rol_id", referencedColumnName = "rolId"))
     private Collection<Rol> roles;
 
     public User() {
