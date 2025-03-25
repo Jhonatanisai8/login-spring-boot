5415ff162cfebf18a6074792b3a0f4d037578478
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-25 08:11:12 -0500

    controlador para el registro de un nuevo usuario

---
+++ src/main/java/com/isai/login_spring_boot/controller/RegistrerUserController.java
@@ -0,0 +1,37 @@
+package com.isai.login_spring_boot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.isai.login_spring_boot.dto.UserRequest;
+import com.isai.login_spring_boot.service.impl.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrerUserController {
+
+    @Autowired
+    private UserService userService;
+
+    @ModelAttribute("user")
+    public UserRequest userRequest() {
+        UserRequest user = new UserRequest();
+        return user;
+    }
+
+    @GetMapping
+    public String showFormRegistrer() {
+        return "register";
+    }
+
+    @PostMapping("path")
+    public String registerUserAccount(@ModelAttribute("user") UserRequest userRequest) {
+        userService.saveUser(userRequest);
+        return "redirect:/register?Exito";
+    }
+
+}
