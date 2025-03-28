b971bf58cd9bb7248c90f9fda69d194fbc07978c
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-28 17:09:53 -0500

    Clase SecurityConfig

---
+++ src/main/java/com/isai/login_spring_boot/security/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package com.isai.login_spring_boot.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration
+ extends WebSecurityConfigurerAdapter {
+
+    
+}
